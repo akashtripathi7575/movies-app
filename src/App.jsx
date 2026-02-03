adf58422bab37b578469b5fe57b983cf0c8b9f44
Akash Tripathi <aakashtripathi54@gmail.com>
2026-02-03 23:59:23 +0530

    movie-search app setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Search from './components/Search.jsx';
+
+const App = () => {
+  const[searchTerm, setSearchTerm] = userState('');
+
+
+  return (
+    <main> 
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+        <Search />
+      </div>
+    </main>
+  )
+}
+
+export default App;
